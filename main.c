2636846d904be6d4b150fd369b9260d37d4ce11c
TiagoQuattrocchi <101597256+TiagoQuattrocchi@users.noreply.github.com>
2023-06-06 17:08:54 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+/*
+ *  @brief  Inicializa los pines del ultrasonico
+ *
+ *  @param  trigger: numero de GP para usar de TRIGGER
+ *  @param  echo: numero de GP para usar de ECHO
+ */
+void ultrasonico_init(uint8_t trigger, uint8_t echo) {
+    /* Habilito el pin de Trigger */
+    gpio_init(trigger);  
+    /* Configuro el Trigger como salida */
+    gpio_set_dir(trigger, true);
+    /* Pongo un 0 en el pin de Trigger */
+    gpio_put(trigger, false);
+    /* Habilito el pin de Echo */
+    gpio_init(echo);
+    /* Configuro el Echo como entrada */
+    gpio_set_dir(echo, false);
+    gpio_init(2);
+    gpio_set_dir(2,true);
+}
+
+/*
+ *  @brief  Obtiene la distancia en cm
+ *  
+ *  @param  trigger: numero de GP para usar de TRIGGER
+ *  @param  echo: numero de GP para usar de ECHO
+ * 
+ *  @return distancia en cm
+ */
+float ultrasonico_get_distance_cm(uint8_t 1, uint8_t 0) {
+    /* Escribo un 1 en el Trigger */
+    gpio_put(trigger, true);
+    /* Espero 10 us con el pulso encendido */
+    sleep_us(10);
+    /* Escribo un 0 en el Trigger */
+    gpio_put(trigger, false);
+    /* Espero a que el pulso llegue al Echo */
+    while(!gpio_get(echo));
+    /* Mido el tiempo de cuando llega el pulso */
+    absolute_time_t from = get_absolute_time();
+    /* Espero a que el pulso del Echo baje a cero */
+    while(gpio_get(echo));
+    /* Mido el tiempo cuando termina el pulso */
+    absolute_time_t to = get_absolute_time();
+    /* Saco la diferencia de tiempo (el ancho del pulso) en us */
+    int64_t pulse_width = absolute_time_diff_us(from, to);
+    /* Calculo la distancia y la devuelvo */
+    return pulse_width / 59.0;
+}
+/*
+ * @brief Programa principal
+ */
+int main(void) {
+    // Inicializacion del USB
+    stdio_init_all();
+    // Elegir un GPIO para trigger, echo y buzzer
+
+    // Inicializacion del ultrasonico
+    ultrasonico_init(trig, echo);
+    // Inicializo buzzer
+
+
+    while(1) {
+        // Hago una lectura de distancia
+        float d = ultrasonico_get_distance_cm(trig, echo);
+        // Muestro por consola
+        if(d<10){
+            gpio_put(2,true);
+        }
+        else{
+            gpio_put(2,false);
+        }
+        // Verifico distancia
+            printf("distancia: %.2f cm", d);
+        // Espero
+        sleep_ms(10);
+    }
+}
